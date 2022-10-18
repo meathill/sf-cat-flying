c8553d54437f04af4a947485b7e603353e4d7c5f
Meathill <meathill@gmail.com>
2022-10-18 23:00:26 +0800

    change: added version

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import {ref} from "vue";
 import GameStage from "./components/stage.vue";
+import pkg from '../package.json';
 
 const hasPanel = ref(true);
 const stage = ref(null);
@@ -17,9 +18,11 @@ game-stage(
   @game-end="hasPanel = true"
 )
 
-.w-48.fixed.bg-white.p-4.top-12.flex.justify-center.items-center.shadow-lg.rounded(
+.w-48.fixed.bg-white.p-4.shadow-lg.rounded(
   v-if="hasPanel"
-  class="top-1/2 left-1/2 -ml-24"
+  class="left-1/2 bottom-1/2 -ml-24"
 )
   button.btn.btn-primary.btn-block(@click="startGame") 开始游戏
+
+  p.text-sm.text-center.text-gray-400.mt-2 v{{pkg.version}}
 </template>
